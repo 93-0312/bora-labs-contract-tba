c897f50919156b22cd471c903c2ebda172c16fe8
Hoang Vi <dhvi1@tma.com.vn>
2023-10-27 11:07:26 +0700

    BoralabsTBA1155: add more unit test for tbaMint and burn

--- test/BoralabsTBA1155-unit.ts
+++ test/BoralabsTBA1155-unit.ts
@@ -28,6 +28,7 @@ describe("BoralabsTBA6551: Unit test", function () {
   let tbaAddress: string;
   let tba: BoralabsTBA6551Account;
   let tbaAddress2: string;
+  let tba2: BoralabsTBA6551Account;
 
   let owner721: HardhatEthersSigner;
   let owner1155: HardhatEthersSigner;
@@ -43,6 +44,9 @@ describe("BoralabsTBA6551: Unit test", function () {
   const iface1155 = new Interface([
     "function safeTransferFrom(address from, address to, uint256 tokenId, uint256 amount, bytes data)",
     "function safeBatchTransferFrom( address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data )",
+    "function tbaMint(address to, uint256 amount, bytes memory data)",
+    "function burn(uint256 id, uint256 amount)",
+    "function setApprovalForAll(address operator, bool approved)",
   ]);
 
   const amount = 10;
@@ -892,6 +896,479 @@ describe("BoralabsTBA6551: Unit test", function () {
         "50000001 balance:",
         await bora1155.balanceOf(User1.address, 50000001)
       );
+
+      this.mlog.after(
+        "[User 2]",
+        "10000001 balance:",
+        await bora1155.balanceOf(User2.address, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(User2.address, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(User2.address, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(User2.address, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(User2.address, 50000001)
+      );
+    });
+  });
+
+  describe("TBA Mint", async function () {
+    it("Should mint successful when minting for itself", async function () {
+      this.mlog.before(
+        "[TBA Account]",
+        "balance:",
+        await bora1155.tokenCountOf(tbaAddress)
+      );
+
+      // Step 1: TBA account uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for itself with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "call execute() to mint tokens ERC1155 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        tbaAddress,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 2: Verify token balance of TBA account are
+      // 10000001, 20000001, 30000001, 40000001, 50000001 with the amount of each tokenId is 10.
+      expect(await bora1155.tokensOf(tbaAddress)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [10, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[TBA Account]",
+        "10000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 50000001)
+      );
+    });
+
+    it("Should TBA account mint successful when minting for other wallet", async function () {
+      this.mlog.before(
+        "[User 1]",
+        "balance:",
+        await bora1155.tokenCountOf(User1.address)
+      );
+
+      // Step 1: TBA account uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for User 1 with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "call execute() to mint tokens ERC1155 for User 1 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        User1.address,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 2: Verify token balance of User 1 are 10000001, 20000001, 30000001, 40000001, 50000001
+      // with the amount of each tokenId is 10.
+      expect(await bora1155.tokensOf(User1.address)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [10, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[User 1]",
+        "10000001 balance:",
+        await bora1155.balanceOf(User1.address, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(User1.address, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(User1.address, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(User1.address, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(User1.address, 50000001)
+      );
+    });
+
+    it("Should mint successful when minting for other TBA account", async function () {
+      // Step 1: User 1 creates a TBA account 2 with tokenId 20000001.
+      await bora6551Registry
+        .connect(User1)
+        .createAccount(
+          bora6551Account.getAddress(),
+          network.config.chainId as BigNumberish,
+          bora721.getAddress(),
+          20000001,
+          0,
+          emptyData
+        );
+
+      // Get TBA account 2 address
+      tbaAddress2 = await bora6551Registry.account(
+        bora6551Account.getAddress(),
+        network.config.chainId as BigNumberish,
+        bora721.getAddress(),
+        20000001,
+        0
+      );
+
+      this.mlog.before(
+        "[TBA Account 2]",
+        "balance:",
+        await bora1155.tokenCountOf(tbaAddress2)
+      );
+
+      // Step 2: TBA account 1 uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for TBA account 2 with amount is 10.
+      this.mlog.log(
+        "[TBA Account 1]",
+        "call execute() to mint tokens ERC1155 for TBA account 2 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        tbaAddress2,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 3: Verify token balance of TBA account 2 are 10000001, 20000001, 30000001, 40000001, 50000001
+      // with the amount of each tokenId is 10.
+      expect(await bora1155.tokensOf(tbaAddress2)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [10, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[TBA Account 2]",
+        "10000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 50000001)
+      );
+    });
+  });
+
+  describe("Burn", async function () {
+    it("Should be successful when burning token of itself", async function () {
+      this.mlog.before(
+        "[TBA Account]",
+        "balance:",
+        await bora1155.tokenCountOf(tbaAddress)
+      );
+
+      // Step 1: TBA account uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for itself with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "call execute() to mint tokens ERC1155 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        tbaAddress,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 2: TBA account using execute() to burn ERC1155 token id is 10000001 with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "uses execute() to burn ERC1155 token id is 10000001 with amount is 10."
+      );
+      data = iface1155.encodeFunctionData("burn", [10000001, amount]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 3: Verify token balance of tokens id 20000001, 30000001, 40000001, 50000001 is 10.
+      // Step 4: Verify token balance of token id 10000001 is 0.
+      expect(await bora1155.tokensOf(tbaAddress)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [0, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[TBA Account]",
+        "10000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(tbaAddress, 50000001)
+      );
+    });
+
+    it("Should be successful when burning token of other wallet", async function () {
+      this.mlog.before(
+        "[User 1]",
+        "balance:",
+        await bora1155.tokenCountOf(User1.address)
+      );
+
+      // Step 1: TBA account uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for User 1 with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "call execute() to mint tokens ERC1155 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        User1.address,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 2: User 1 calls setApprovalForAll() to approve all for TBA account.
+      this.mlog.log(
+        "[User 1]",
+        "calls setApprovalForAll() to to approve all for TBA account."
+      );
+      await bora1155.connect(User1).setApprovalForAll(tbaAddress, true);
+
+      // Step 3: TBA account using execute() to burn ERC1155 token id is 10000001 with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "uses execute() to burn ERC1155 token id is 10000001 with amount is 10."
+      );
+      data = iface1155.encodeFunctionData("burn", [10000001, amount]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 3: Verify token balance of tokens id 20000001, 30000001, 40000001, 50000001 is 10.
+      // Step 4: Verify token balance of token id 10000001 is 0.
+      expect(await bora1155.tokensOf(User1.address)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [0, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[User 1]",
+        "10000001 balance:",
+        await bora1155.balanceOf(User1.address, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(User1.address, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(User1.address, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(User1.address, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(User1.address, 50000001)
+      );
+    });
+
+    it("Should be successful when burning token of other TBA account", async function () {
+      // Step 1: User 1 creates a TBA account 2 with tokenId 20000001.
+      await bora6551Registry
+        .connect(User1)
+        .createAccount(
+          bora6551Account.getAddress(),
+          network.config.chainId as BigNumberish,
+          bora721.getAddress(),
+          20000001,
+          0,
+          emptyData
+        );
+
+      // Get TBA account 2 address
+      tbaAddress2 = await bora6551Registry.account(
+        bora6551Account.getAddress(),
+        network.config.chainId as BigNumberish,
+        bora721.getAddress(),
+        20000001,
+        0
+      );
+
+      tba2 = await ethers.getContractAt("BoralabsTBA6551Account", tbaAddress2);
+
+      this.mlog.before(
+        "[TBA Account 2]",
+        "balance:",
+        await bora1155.tokenCountOf(tbaAddress2)
+      );
+
+      // Step 2: TBA account 1 uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for TBA account 2 with amount is 10.
+      this.mlog.log(
+        "[TBA Account 1]",
+        "call execute() to mint tokens ERC1155 for TBA account 2 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        tbaAddress2,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 3: TBA account 2 uses execute() to call setApprovalForAll() to approve all for TBA account 1.
+      this.mlog.log(
+        "[TBA Account 2]",
+        "uses execute() to call setApprovalForAll() to approve all for TBA account 1."
+      );
+      data = iface1155.encodeFunctionData("setApprovalForAll", [
+        tbaAddress,
+        true,
+      ]);
+      await tba2.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 4: TBA account 1 using execute() to burn the token of TBA account 2 with id is 10000001
+      // and amount is 10.
+      this.mlog.log(
+        "[TBA Account 1]",
+        "uses execute() to burn the token of TBA account 2 with id is 10000001 and amount is 10."
+      );
+      data = iface1155.encodeFunctionData("burn", [10000001, amount]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 5: Verify token balance of TBA account 2 are 20000001, 30000001, 40000001, 50000001
+      // with the amount of each tokenId is 10.
+      // Step 6: Verify token balance of token id 10000001 is 0.
+      expect(await bora1155.tokensOf(tbaAddress2)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [0, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[TBA Account 2]",
+        "10000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 50000001)
+      );
+    });
+
+    it("Should failed when burning token of other TBA account without approval", async function () {
+      // Step 1: User 1 creates a TBA account 2 with tokenId 20000001.
+      await bora6551Registry
+        .connect(User1)
+        .createAccount(
+          bora6551Account.getAddress(),
+          network.config.chainId as BigNumberish,
+          bora721.getAddress(),
+          20000001,
+          0,
+          emptyData
+        );
+
+      // Get TBA account 2 address
+      tbaAddress2 = await bora6551Registry.account(
+        bora6551Account.getAddress(),
+        network.config.chainId as BigNumberish,
+        bora721.getAddress(),
+        20000001,
+        0
+      );
+
+      tba2 = await ethers.getContractAt("BoralabsTBA6551Account", tbaAddress2);
+
+      this.mlog.before(
+        "[TBA Account 2]",
+        "balance:",
+        await bora1155.tokenCountOf(tbaAddress2)
+      );
+
+      // Step 2: TBA account 1 uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for TBA account 2 with amount is 10.
+      this.mlog.log(
+        "[TBA Account 1]",
+        "call execute() to mint tokens ERC1155 for TBA account 2 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        tbaAddress2,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 3: TBA account 1 using execute() to burn the token of TBA account 2
+      // with id is 10000001 and amount is 10.
+      // Step 4: Verify transaction will be reverted with error message “Caller is not token owner or approved”.
+      this.mlog.log(
+        "[TBA Account 1]",
+        "uses execute() to burn the token of TBA account 2 with id is 10000001 and amount is 10."
+      );
+      data = iface1155.encodeFunctionData("burn", [10000001, amount]);
+      await expect(
+        tba.connect(User1).execute(bora1155.target, 0, data, 0)
+      ).revertedWith("Caller is not token owner or approved");
+
+      // Step 5: Verify token balance of TBA account 2 is not changed.
+      expect(await bora1155.tokensOf(tbaAddress2)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [10, 10, 10, 10, 10],
+      ]);
+
+      this.mlog.after(
+        "[TBA Account 2]",
+        "10000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 10000001),
+        "20000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 20000001),
+        "30000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 30000001),
+        "40000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 40000001),
+        "50000001 balance:",
+        await bora1155.balanceOf(tbaAddress2, 50000001)
+      );
+    });
+
+    it("Should failed when burning token of other wallet without approval", async function () {
+      this.mlog.before(
+        "[User 2]",
+        "balance:",
+        await bora1155.tokenCountOf(User2.address)
+      );
+
+      // Step 1: TBA account uses execute() to call tbaMint() to mint token ERC1155 ids
+      // (10000001, 20000001, 30000001, 40000001 and 50000001) for User 2 with amount is 10.
+      this.mlog.log(
+        "[TBA Account]",
+        "call execute() to mint tokens ERC1155 for User 2 with an amount of each is 10"
+      );
+      data = iface1155.encodeFunctionData("tbaMint", [
+        User2.address,
+        amount,
+        emptyData,
+      ]);
+      await tba.connect(User1).execute(bora1155.target, 0, data, 0);
+
+      // Step 2: TBA account using execute() to burn the token of User 2
+      // with id is 10000001 and amount is 10.
+      // Step 3: Verify transaction will be reverted with error message “Caller is not token owner or approved”.
+      this.mlog.log(
+        "[TBA Account]",
+        "uses execute() to burn the token of User 2 with id is 10000001 and amount is 10."
+      );
+      data = iface1155.encodeFunctionData("burn", [10000001, amount]);
+      await expect(
+        tba.connect(User1).execute(bora1155.target, 0, data, 0)
+      ).revertedWith("Caller is not token owner or approved");
+
+      // Step 4: Verify token balance of User 2 is not changed.
+      expect(await bora1155.tokensOf(User2.address)).to.deep.equal([
+        [10000001, 20000001, 30000001, 40000001, 50000001],
+        [10, 10, 10, 10, 10],
+      ]);
 
       this.mlog.after(
         "[User 2]",
